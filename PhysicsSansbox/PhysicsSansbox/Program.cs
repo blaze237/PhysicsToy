0f9fceeb72eb1c38c7d7f0613a0b83df798ea855
Danni <danwhittaker22@gmail.com>
2026-02-14 14:57:27 +0000

    Initial project config

---
+++ PhysicsSansbox/PhysicsSansbox/Program.cs
@@ -0,0 +1,27 @@
+﻿using Raylib_cs;
+using static Raylib_cs.Raylib;
+using static Raylib_cs.Color;
+
+class Program
+{
+    static void Main()
+    {
+        const int screenWidth = 800;
+        const int screenHeight = 600;
+
+        InitWindow(screenWidth, screenHeight, "Raylib C# Sandbox");
+        SetTargetFPS(60);
+
+        while (!WindowShouldClose())
+        {
+            BeginDrawing();
+            ClearBackground(Raylib_cs.Color.Black);
+
+            DrawText("It works!", 320, 280, 20, Raylib_cs.Color.RayWhite);
+
+            EndDrawing();
+        }
+
+        CloseWindow();
+    }
+}
